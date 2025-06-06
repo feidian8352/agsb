5042daff050a4b954b085945d3472c805ba8b960
feidian8352 <43678136+feidian8352@users.noreply.github.com>
2025-06-06 11:49:26 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "feidian"         # 用户名
+UUID = "9c887237-b783-4107-9bfc-c1b3068a75d5"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "argo2.feidian8352.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiNjZkYzJlMGE4MmU1MDgzN2UwYWYyYjA2ZmZkNGVjYzciLCJ0IjoiMzg0YzRiZmMtNDJiZS00NDYwLTgwZjAtYTJkOTEyODcyNGMzIiwicyI6Ik5URTNZVFl5TURFdFkyUXdOeTAwWlRoaUxXSm1aR1F0TkdReE56QXlNR1JrWWpSaiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
